1ff3466e7a2d677c68e0bf98908f9956118da84b
dev <devendra1andonly@gmail.com>
2023-02-03 17:11:45 +0530

    created order-service
    DB - mysql | port - 8081

---
+++ order-service/src/main/java/com/quickstore/orderservice/service/OrderService.java
@@ -0,0 +1,40 @@
+package com.quickstore.orderservice.service;
+
+import com.quickstore.orderservice.dto.OrderLineItemsDto;
+import com.quickstore.orderservice.dto.OrderRequest;
+import com.quickstore.orderservice.model.Order;
+import com.quickstore.orderservice.model.OrderLineItems;
+import com.quickstore.orderservice.repository.OrderRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+@Transactional
+public class OrderService {
+
+    private final OrderRepository orderRepository;
+
+    public void placeOrder(OrderRequest orderRequest) {
+        Order order = new Order();
+        order.setOrderNumber(UUID.randomUUID().toString());
+        List<OrderLineItems> orderLineItemsList = orderRequest.getOrderLineItemsDtoList()
+                .stream()
+                .map(this::mapToDto)
+                .toList();
+        order.setOrderLineItemsList(orderLineItemsList);
+        orderRepository.save(order);
+    }
+
+    public OrderLineItems mapToDto(OrderLineItemsDto orderLineItemsDto) {
+        OrderLineItems orderLineItems = new OrderLineItems();
+        orderLineItems.setQuantity(orderLineItemsDto.getQuantity());
+        orderLineItems.setPrice(orderLineItemsDto.getPrice());
+        orderLineItems.setSkuCode(orderLineItemsDto.getSkuCode());
+        return orderLineItems;
+    }
+}
