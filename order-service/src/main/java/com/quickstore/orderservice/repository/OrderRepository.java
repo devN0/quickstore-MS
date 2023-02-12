6c5f59a5681d0b8f7bfd616d2ebd9ff3f41933e6
Devendra Diwakar <58916808+devN0@users.noreply.github.com>
2023-02-12 23:00:04 +0530

    Revert "merge main into o-s/check stock status before placing order by sending skuCodes to i-s"

--- order-service/src/main/java/com/quickstore/orderservice/repository/OrderRepository.java
+++
@@ -1,7 +0,0 @@
-package com.quickstore.orderservice.repository;
-
-import com.quickstore.orderservice.model.Order;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrderRepository extends JpaRepository<Order, Long> {
-}
