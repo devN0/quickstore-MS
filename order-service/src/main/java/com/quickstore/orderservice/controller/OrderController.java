acbf2a1dceb4a505f84b66b1b2afcdff06d5c72c
dev <devendra1andonly@gmail.com>
2023-02-25 03:06:27 +0530

    added circuit-breaker b/w orderService and inventoryService
    dependencies added to order-service: Resilience4j, spring-boot-starter-actuator. CircuitBreaker logic applied to placeOrder method in OrderController

--- order-service/src/main/java/com/quickstore/orderservice/controller/OrderController.java
+++ order-service/src/main/java/com/quickstore/orderservice/controller/OrderController.java
@@ -2,6 +2,7 @@ package com.quickstore.orderservice.controller;
 
 import com.quickstore.orderservice.dto.OrderRequest;
 import com.quickstore.orderservice.service.OrderService;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -14,8 +15,13 @@ public class OrderController {
     private final OrderService orderService;
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
+    @CircuitBreaker(name="inventory", fallbackMethod = "fallbackMethod")
     public String placeOrder(@RequestBody OrderRequest orderRequest) {
         orderService.placeOrder(orderRequest);
         return "order placed successfully.";
+    }
+
+    public String fallbackMethod(OrderRequest orderRequest, RuntimeException runtimeException) {
+        return "Oops! something went wrong, please order again later.";
     }
 }
